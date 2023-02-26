e9e4d3a3472a2c887bb809fc51c909bb94de9fad
Mammetnazar <nazar.rudn@gmail.com>
2023-02-26 11:03:19 +0300

    - self.labelValue.text = "Значение счётчика: \(self.counter)" was added to the countAdd function which replaces the initial text - initial text label was changed to "Значение счётчика: 0"

--- Counterio/ViewController.swift
+++ Counterio/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     private var counter: Int = 0
 
     override func viewDidLoad() {
@@ -18,11 +19,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelValue: UILabel!
     
+    
     @IBOutlet weak var countButton: UIButton!
     
     @IBAction func countAdd(_ sender: Any) {
         print("Значение счётчика: \(self.counter)")
         counter += 1
+        self.labelValue.text = "Значение счётчика: \(self.counter)"
     }
     
 }
